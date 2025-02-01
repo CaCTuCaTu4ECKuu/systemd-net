ef4b48c9027b941093956e48292962810f2850f0
CaCTuCaTu4ECKuu <CaCTuCaTu4ECKuu@users.noreply.github.com>
2025-02-01 09:44:09 +0300

    Update tool
    * Use tabulate for list
    * Update argparse definition
    * Implement more commands

--- src/modules/application.py
+++ src/modules/application.py
@@ -1,60 +1,77 @@
 import argparse
+from tabulate import tabulate
 
+from modules.core import SVCArgsProp
 from modules.NetService import *
 
+SVC_TABLE_HEADERS = ["Name", "URLs", "Enable", "Active"]
 def print_services(services:list[NetService]):
+    data = []
     if (len(services) > 0):
         for idx,svc in enumerate(services):
-            print(f"{svc.Name}; {svc.ASPNETCORE_URLS}; {svc.get_service_enabled()}; {svc.get_service_active()}")
+            state_enabled = svc.get_service_enabled()
+            state_active = svc.get_service_active()
+            if state_enabled == 'enabled':
+                state_enabled = f"{COLOR_SUCCESS}{state_enabled}{COLOR_BASE}"
+            if state_active == 'active':
+                state_active = f"{COLOR_SUCCESS}{state_active}{COLOR_BASE}"
+            elif state_active == 'failed':
+                state_active = f"{COLOR_DANGER}{state_active}{COLOR_BASE}"
+            elif state_active == 'activating':
+                state_active = f"{COLOR_WARN}{state_active}{COLOR_BASE}"
+            urls = svc.ASPNETCORE_URLS.replace(";","\n")
+
+            data.append([svc.Name, urls, state_enabled, state_active])
+        print(tabulate(data, headers=SVC_TABLE_HEADERS, tablefmt="grid"))
     else:
         print("No services registered.")
 
 def handle_command(args:argparse.Namespace):
     # List registered services
-    if args.list:
+    if args.command == 'list':
         services = get_services(args.service_dir, args.prefix)
         print_services(services)
-        exit(1)
+        exit()
 
     # Delete service
-    if args.delete:
-        svc_name = str(args.service_name)
-        if not svc_name.startswith(args.prefix):
-            svc_name = f"{args.prefix}{svc_name}"
-
-        svc_path = os.path.join(args.service_dir, svc_name)
-        if not svc_path.endswith('.service'):
-            svc_path = f"{svc_path}.service"
-        if not os.path.isfile(svc_path):
-            print(f"Service not found - {svc_path}")
+    if args.command == 'del':
+        svcArgs = SVCArgsProp(args.service_dir, args.prefix, args.service_name)
+        if not os.path.isfile(svcArgs.Path):
+            print(f"Service not found - {svcArgs.Path}")
             exit(1)
-        svc = read_service(svc_path)
+
+        svc = read_service(svcArgs.Path)
         if svc.get_service_active() == 'active' and not args.force:
             print(f"{COLOR_DANGER}Service is currently active. Use --force to stop and delete service.{COLOR_BASE}")
             exit(1)
         print(f"Deleting service '{svc.Name}' from systemd...")
-        delete_service(svc_path)
+        delete_service(svcArgs.Path, args.cleanup)
         print(f"{COLOR_SUCCESS}Service '{svc.Name}' deleted from systemd{COLOR_BASE}")
-        exit(1)
+        exit()
 
     # Register net service
-    if args.add:
-        svc_name = str(args.service_name)
-        if not svc_name.startswith(args.prefix):
-            svc_name = f"{args.prefix}{svc_name}"
-
-        svc_path = os.path.join(args.service_dir, svc_name)
-        if not svc_path.endswith('.service'):
-            svc_path = f"{svc_path}.service"
-        if os.path.isfile(svc_path):
-            print(f"{COLOR_DANGER}Service already exists - {svc_path}{COLOR_BASE}")
-            exit(1)
+    if args.command == 'add':
         if args.exec_path is None or len(str(args.exec_path).strip()) == 0:
             print(f"{COLOR_DANGER}Service execution path required (-exec){COLOR_BASE}")
+            exit(2)
+
+        svcArgs = SVCArgsProp(args.service_dir, args.prefix, args.service_name)
+        if os.path.isfile(svcArgs.Path):
+            print(f"{COLOR_DANGER}Service already exists - {svcArgs.Path}{COLOR_BASE}")
             exit(1)
 
-        svc = create_service_blank(args.service_dir, svc_name)
+        svc = create_service_blank(args.service_dir, svcArgs.Name)
         svc.Params.set_ExecStart(args.exec_path)
+        if (args.working_dir and len(args.working_dir) > 0):
+            if os.path.isdir(args.working_dir):
+                svc.Params.Properties['WorkingDirectory'] = args.working_dir
+            else:
+                print(f"{COLOR_DANGER}Path is not directory - '{args.working_dir}'{COLOR_BASE}")
+                exit(1)
+        
+        if (args.user and len(args.user) > 0):
+            svc.Params.Properties['User'] = args.user
+            svc.Params.Properties['Group'] = args.user
 
         if args.aspnetcore_urls is not None and len(str(args.aspnetcore_urls)) > 0:
             svc.ASPNETCORE_URLS = args.aspnetcore_urls # TODO: autofill defaults
@@ -62,4 +79,48 @@ def handle_command(args:argparse.Namespace):
 
         svc.try_save(args.service_dir)
         print(f"{COLOR_SUCCESS}Service {svc.Name} added to systemd - {svc.ServicePath}{COLOR_BASE}")
-        exit(1)
+        exit()
+    
+    if args.command == 'edit':
+        app = str(args.app)
+        svcArgs = SVCArgsProp(args.service_dir, args.prefix, args.service_name)
+        if not os.path.isfile(svcArgs.Path):
+            print(f"Service not found - {svcArgs.Path}")
+            exit(1)
+
+        if args.env:
+            svc = read_service(svcArgs.Path)
+            if not os.path.isfile(svc.EnvironmentFile):
+                print(f"Environment file not found - {svc.EnvironmentFile}")
+                exit(1)
+            subprocess.run([app, svc.EnvironmentFile])
+            exit()
+
+        subprocess.run([app, svcArgs.Path])
+        exit()
+    
+    if args.command == 'start':
+        svcArgs = SVCArgsProp(args.service_dir, args.prefix, args.service_name)
+        if not os.path.isfile(svcArgs.Path):
+            print(f"Service not found - {svcArgs.Path}")
+            exit(1)
+        
+        svc = read_service(svcArgs.Path)
+        svc_state = svc.get_service_active()
+        if (svc_state != 'active') and (svc_state != 'activating'):
+            subprocess.run(['systemctl', 'start', svcArgs.FullName], text=True, check=True)
+        
+        exit()
+    
+    if args.command == 'stop':
+        svcArgs = SVCArgsProp(args.service_dir, args.prefix, args.service_name)
+        if not os.path.isfile(svcArgs.Path):
+            print(f"Service not found - {svcArgs.Path}")
+            exit(1)
+
+        svc = read_service(svcArgs.Path)
+        svc_state = svc.get_service_active()
+        if (svc_state == 'active') or (svc_state == 'activating'):
+            subprocess.run(['systemctl', 'stop', svcArgs.FullName], text=True, check=True)
+        
+        exit()
--- src/modules/core.py
+++ src/modules/core.py
@@ -12,17 +12,66 @@ COLOR_DANGER = '\033[91m'   # RED
 TOOL_FILEGEN_COMMENT = "# This file was automatically created using systemd-net utility"
 
 app_argsparser = argparse.ArgumentParser(prog='systemd-net', description=f"{COLOR_INFO}systemd-net is utility to easy manage your .NET Applications services{COLOR_BASE}")
-app_argsparser.add_argument('-s', '--service-dir', type=str, help="", default="/etc/systemd/system/")
-app_argsparser.add_argument('-e', '--env-dir', type=str, help="", default="/etc/systemd/system/env/")
-app_argsparser.add_argument('-p', '--prefix', type=str, help="Prefix to filter services", default="netapp.")
-app_argsparser.add_argument('-F', '--force', help="Force actoin", action="store_true")
-app_argsparser.add_argument('-L', '--list', help="List of registered applications", action="store_true")
-app_argsparser.add_argument('-D', '--delete', help="Delete registered service from systemd", action="store_true")
-app_argsparser.add_argument('-A', '--add', help="Register new service", action='store_true')
-app_argsparser.add_argument('-svc', '--service-name', help="Name of service to do action", default=None)
-app_argsparser.add_argument('-exec', '--exec-path', type=str, help="Path to application executable (.dll)", default=None)
-app_argsparser.add_argument('--aspnetcore-urls', type=str, help="ASPNETCORE_URLS environment variable", default=None)
-app_argsparser.add_argument('--aspnetcore-env', type=str, help="ASPNETCORE_ENVIRONMENT environment variable", default="Production")
+app_cmdparser = app_argsparser.add_subparsers(dest='command', help='Avaliable commands')
+
+common_parser = argparse.ArgumentParser(add_help=False)
+common_parser.add_argument('-p', '--prefix', type=str, help="Prefix to filter services\n(default: '%(default)s')", default="netapp.")
+
+app_addparser = app_cmdparser.add_parser('add', parents=[common_parser], help='Add application')
+app_addparser.add_argument('service_name', type=str, help='Name of service in systemd')
+app_addparser.add_argument('exec_path', type=str, help="Path to application executable (.dll)")
+app_addparser.add_argument('-sdir', '--service-dir', type=str, help=".service files directory\n(default: %(default)s)", default="/etc/systemd/system/")
+app_addparser.add_argument('-edir', '--env-dir', type=str, help=".env files directory\n(default: %(default)s)", default="/etc/systemd/system/env/")
+app_addparser.add_argument('-wdir', '--working-dir', help="Application working directory\n(default same as 'exec_path' directory)")
+app_addparser.add_argument('-u', '--user', help='User to run application\n(default: %(default)s)', default='www-data')
+app_addparser.add_argument('--aspnetcore-env', type=str, help="ASPNETCORE_ENVIRONMENT environment variable (default: '%(default)s')", default="Production")
+app_addparser.add_argument('--aspnetcore-urls', type=str, help="ASPNETCORE_URLS environment variable", default=None)
+
+app_delparser = app_cmdparser.add_parser('del', parents=[common_parser], help='Delete application')
+app_delparser.add_argument('service_name', help='Name of service in systemd (with prefix)')
+app_delparser.add_argument('-sdir', '--service-dir', type=str, help=".service files directory\n(default: %(default)s)", default="/etc/systemd/system/")
+app_delparser.add_argument('-F', '--force', help="Force deletion", action="store_true")
+app_delparser.add_argument('--cleanup', help="Delete service and environment .bak files\n(default is false)", action="store_true", default=False)
+
+app_listparser = app_cmdparser.add_parser('list', parents=[common_parser], help='List registered applications')
+app_listparser.add_argument('-sdir', '--service-dir', type=str, help=".service files directory\n(default: %(default)s)", default="/etc/systemd/system/")
+
+app_editparser = app_cmdparser.add_parser('edit', parents=[common_parser], help="Edit with text editor")
+app_editparser.add_argument('app', help="Application of your choise to use for edit ('vi', 'nano', etc.)")
+app_editparser.add_argument('service_name', help="Name of service in systemd")
+app_editparser.add_argument('-sdir', '--service-dir', type=str, help=".service files directory\n(default: %(default)s)", default="/etc/systemd/system/")
+app_editparser.add_argument('--env', help="Edit environment file defined in service", action="store_true")
+
+app_startparser = app_cmdparser.add_parser('start', parents=[common_parser], help="Start application")
+app_startparser.add_argument('service_name', type=str, help='Name of service in systemd')
+app_startparser.add_argument('-sdir', '--service-dir', type=str, help=".service files directory\n(default: %(default)s)", default="/etc/systemd/system/")
+
+app_stopparser = app_cmdparser.add_parser('stop', parents=[common_parser], help="Stop application")
+app_stopparser.add_argument('service_name', type=str, help='Name of service in systemd')
+app_stopparser.add_argument('-sdir', '--service-dir', type=str, help=".service files directory\n(default: %(default)s)", default="/etc/systemd/system/")
+
+class SVCArgsProp:
+    ServiceDir:str
+    Prefix:str
+    Name:str
+    FullName:str
+    Path:str
+    
+    def __init__(self, sdir, prefix, name):
+        self.ServiceDir = str(sdir)
+        self.Prefix = str(prefix)
+        self.Name = str(name)
+
+        if not self.Name.startswith(self.Prefix):
+            self.FullName = f"{self.Prefix}{self.Name}"
+        else:
+            self.FullName = self.Name
+            self.Name = self.Name.removeprefix(self.Prefix)
+        
+        self.Path = os.path.join(self.ServiceDir, self.FullName)
+        if not self.Path.endswith('.service'):
+            self.Path = f"{self.Path}.service"
+        
 
 def is_dotnet_installed(dotnet_cli_path:str=None):
     if dotnet_cli_path is not None:
